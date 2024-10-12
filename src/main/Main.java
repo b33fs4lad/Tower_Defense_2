a7a225ce31880f021c3e6da07151b386a118f250
ikixu <im.allysa110@gmail.com>
2024-10-12 15:03:40 -0500

    new window size

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,9 +8,13 @@ public class Main extends JFrame {
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocation(300, 150);
+
+        GameScreen gameScreen = new GameScreen();
+        add(gameScreen);
     }
 
         public static void main(String[] args) {
         System.out.println("bro");
+        Main main = new Main();
         }
 }
