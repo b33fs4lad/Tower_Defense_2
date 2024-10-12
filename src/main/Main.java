3515ef0fd445da5178de7784b4e225115240a55f
ikixu <im.allysa110@gmail.com>
2024-10-12 14:13:49 -0500

    new window size

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,12 +4,14 @@ import javax.swing.JFrame;
 
 public class Main extends JFrame {
     public Main () {
-        setSize(800, 600);
+        setSize(768, 576);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocation(300,150);
     }
 
         public static void main(String[] args) {
-        System.out.println("bro");
+            System.out.println("bro");
+
         }
 }
