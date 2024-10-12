744d4eda2dbce2252b4deca3c26d64dce4344ac6
ikixu <im.allysa110@gmail.com>
2024-10-12 13:13:36 -0500

    window

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+    public Main () {
+        setSize(800, 600);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+        public static void main(String[] args) {
+        System.out.println("bro");
+        }
+}
