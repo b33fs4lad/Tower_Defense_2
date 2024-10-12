e89ed6cfe3c80a13ebea9065e49f429e424e1cfa
Roldham02 <reed.oldham@hotmail.com>
2024-10-12 14:10:20 -0500

    resource folder added

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,13 +3,15 @@ package main;
 import javax.swing.JFrame;
 
 public class Main extends JFrame {
-    public Main () {
+    public Main() {
         setSize(800, 600);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("bro");
-        }
+
+    Main frame = new Main();
+    }
 }
